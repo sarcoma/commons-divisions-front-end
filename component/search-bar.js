30d9d9c22145ae07854ca960cb37b5ae8d155bbe
sarcoma <sarcoma@live.co.uk>
2019-06-09 00:51:46 +0100

    Playing with sass setting overrides.

--- component/search-bar.js
+++ component/search-bar.js
@@ -13,21 +13,23 @@ export const SearchBar = (
         error = false,
         ...props
     }) =>
-    <Field
-        className={'form-field--search form-field--inline ' + className}
-        type='text'
-        error={error}
-    >
-        <Input
-            className={'input--search border-radius--right--flat ' +
-            className} error={error} label='Search'
-            {...props}
-        />
-        <ButtonIcon
-            aliaLabel='Search'
-            icon={<FontAwesomeIcon icon={['fas', 'search']}/>}
-            onClick={onClick}
-            className={'border-radius--left--flat'}
-        />
-    </Field>
+    <form>
+        <Field
+            className={'form-field--search form-field--inline ' + className}
+            type='text'
+            error={error}
+        >
+            <Input
+                className={'input--search border-radius--right--flat ' +
+                className} error={error} label='Search'
+                {...props}
+            />
+            <ButtonIcon
+                ariaLabel='Search'
+                icon={<FontAwesomeIcon icon={['fas', 'search']}/>}
+                onClick={onClick}
+                className={'border-radius--left--flat'}
+            />
+        </Field>
+    </form>
 ;

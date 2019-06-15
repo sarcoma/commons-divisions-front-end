ebf4c3a380d0c3322ca10ea5b6af6232a6d0d5f1
sarcoma <sarcoma@live.co.uk>
2019-06-15 18:09:52 +0100

    Add font + colours

--- component/meta.js
+++ component/meta.js
@@ -10,7 +10,8 @@ export default () => (
         <Head>
             <title>House of Commons Divisions</title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta charSet="utf-8" />
+            <meta charSet="utf-8" />
+            <link href="https://fonts.googleapis.com/css?family=Vollkorn:400,700&display=swap&subset=latin-ext" rel="stylesheet" />
         </Head>
       </div>
 )

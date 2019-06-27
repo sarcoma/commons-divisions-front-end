2f09ca5e39d2b476eba8d31e7ee655842c4c1c49
sarcoma <sarcoma@live.co.uk>
2019-06-28 00:03:12 +0100

    Add colours to bar charts + remove time from date

--- pages/commons-division.js
+++ pages/commons-division.js
@@ -30,9 +30,76 @@ function CommonsDivision({commonsDivision}) {
         </div>;
     });
 
+    const filterVoteData = (vote) => commonsDivision.votes.filter(
+        cd => cd.vote === vote).map(cd => {
+        return cd.member_of_parliament;
+    });
+
     const ayeVotes = filterByVote('aye');
     const noVotes = filterByVote('no');
     const didNotVote = filterByVote('no_vote');
+
+
+    const ayeVotesData = filterVoteData('aye');
+    const noVotesData = filterVoteData('no');
+    const didNotVoteData = filterVoteData('no_vote');
+
+    const voteBar = votes => {
+        const splitVotes = votes.reduce((acc, vote) => {
+            if(!acc.hasOwnProperty(vote.party)) {
+                acc[vote.party] = {votes: 1};
+            } else {
+                acc[vote.party].votes++;
+            }
+            return acc;
+        }, {});
+
+        const sections = [];
+        for(const party in splitVotes) {
+            if(splitVotes.hasOwnProperty(party)) {
+                sections.push(voteSection(party, splitVotes[party].votes));
+            }
+        }
+        return sections;
+    };
+
+    const voteSection = (party, votes)  => <div
+        title={party}
+        style={{
+            height: '60px',
+            width: ((votes / commonsDivision.votes.length) * 100) + '%',
+        }} className={[getPartyColour(party), "vote-section"].join(' ')}
+    />;
+
+    const getPartyColour = (party) => {
+        switch(party) {
+            case "Labour":
+            case "Labour (Co-op)":
+                return "labour";
+            case "Conservative":
+                return "tory";
+            case "Liberal Democrat":
+                return "lib-dem";
+            case "Green Party":
+                return "green-party";
+            case "Scottish National Party":
+                return "snp";
+            case "Sinn Féin":
+                return "sinn-fein";
+            case "Democratic Unionist Party":
+                return "dup";
+            case "Change UK - The Independent Group":
+                return "change-uk";
+            case "Plaid Cymru":
+                return "plaid-cymru";
+            case "Brexit Party":
+                return "brexit-party";
+            case "UK Independence Party":
+                return "ukip";
+            default:
+                return 'independent';
+        }
+    };
 
     return (
         <Page>
@@ -40,7 +107,7 @@ function CommonsDivision({commonsDivision}) {
                 <Row>
                     <Column>
                         <Title tag="h2">{commonsDivision.title}</Title>
-                        <Text>{commonsDivision.date}</Text>
+                        <Text>{commonsDivision.date.replace(/\d{2}:\d{2}:\d{2} GMT/i, '')}</Text>
                         <Text className={'text--semi-bold'}>{
                             ayeVotes.length > noVotes.length
                                 ? 'The ayes have it'
@@ -55,40 +122,25 @@ function CommonsDivision({commonsDivision}) {
                         <Row>
                             <Column span={['1']}>Ayes</Column>
                             <Column span={['11']}>
-                                <div
-                                    style={{
-                                        height: '60px',
-                                        width: ((ayeVotes.length /
-                                            commonsDivision.votes.length) *
-                                            100) + '%',
-                                    }} className="bc--dark-grey"
-                                />
+                                <div className={'vote-bar'}>
+                                    {voteBar(ayeVotesData)}
+                                </div>
                             </Column>
                         </Row>
                         <Row>
                             <Column span={['1']}>Noes</Column>
                             <Column span={['11']}>
-                                <div
-                                    style={{
-                                        height: '60px',
-                                        width: ((noVotes.length /
-                                            commonsDivision.votes.length) *
-                                            100) + '%',
-                                    }} className="bc--darker-grey"
-                                />
+                                <div className={'vote-bar'}>
+                                    {voteBar(noVotesData)}
+                                </div>
                             </Column>
                         </Row>
                         <Row>
-                            <Column span={['1']}>No Vote</Column>
+                            <Column span={['1']}>Did not vote</Column>
                             <Column span={['11']}>
-                                <div
-                                    style={{
-                                        height: '60px',
-                                        width: ((didNotVote.length /
-                                            commonsDivision.votes.length) *
-                                            100) + '%',
-                                    }} className="bc--grey"
-                                />
+                                <div className={'vote-bar'}>
+                                    {voteBar(didNotVoteData)}
+                                </div>
                             </Column>
                         </Row>
                         <Title tag="h3">Votes</Title>
